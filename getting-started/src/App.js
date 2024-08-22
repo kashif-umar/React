100aa9ad5675cc657439f5f2e15cc720e66d8c32
kashif-umar <87655876+kashif-umar@users.noreply.github.com>
2024-08-23 03:32:20 +0530

    Add files via upload

---
+++ getting-started/src/App.js
@@ -0,0 +1,23 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Main Topics in React Learning</h1>
+      <ul>
+        <li>Screens</li>
+        <li>Components</li>
+        <li>Props</li>
+      </ul>
+      <h3>Lifecycle Hooks</h3>
+      <ol>
+        <li>UseState</li>
+        <li>UseRender</li>
+        <li>UseEffect</li>
+      </ol>
+    </div>
+  );
+}
+
+export default App;
